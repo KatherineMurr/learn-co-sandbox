2189d33384f845f4a0f676868153fb7f84f06e85
KatherineMurr <katherinemurr@yahoo.com>
2018-08-01 15:39:10 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,56 @@
+let snake = {
+  hasLegs : false,
+  hasHouse : false
+}
+
+let geico = {
+  hasLegs : true,
+  hasHouse : false
+}
+
+let snail = {
+  hasLegs : false,
+  hasHouse : true
+}
+
+let turtle = {
+  hasLegs : true
+  hasHouse :false
+}
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+
+
